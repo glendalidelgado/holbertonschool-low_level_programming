cc5743e2f1d23f4c6da9a0acf6450aceb36e64c6
glendalidelgado <5363@holbertonschool.com>
2022-07-20 15:04:56 +0000

    Fixing the string

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,24 +1,28 @@
 /*
- * Task 9 Patience, persistence and perspiration
+ * File: 9-print_comb.c
+ * Auth: Glendali Delgado
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints all the numbers of base 16 in lowercase.
+ * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int num;
-	char letter;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
+	{
 		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
 
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
+		putchar(',');
+		putchar(' ');
+	}
 
 	putchar('\n');
 
