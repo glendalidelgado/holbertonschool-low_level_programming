22902a23c6437abe297d87305a38577a12bee44d
glendalidelgado <5363@holbertonschool.com>
2022-07-21 22:51:15 +0000

    fixing the string again

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+/*
+ * File: 101-quote.c
+ * Auth: Glendali Delgado
+ */
+
 #include <unistd.h>
+
 /**
- * main - Write a C program that prints exactly and that piece of art is useful
- * Return: 1 if success.
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
