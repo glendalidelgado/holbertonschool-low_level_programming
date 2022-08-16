3c1736e3289633a0aac08210c3420c8101e2f83f
glendalidelgado <5363@holbertonschool.com>
2022-08-16 02:28:57 +0000

    Write a function that concatenates two strings.

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+char* strcat(char *a, char *b) {
+    while (*a++);
+    a--;
+    while (*a++ = *b++);
+    return a;
+}
+
+char *str_a = "a string";
+char *str_b = "a long long long string";
+char *str_c = "yet another string";
+char *result;
+
+result = strcat(str_a, str_b);
+result = strcat(result, str_c);
