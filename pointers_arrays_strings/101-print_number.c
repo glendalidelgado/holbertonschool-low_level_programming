0d1a69d81ea6c0c1e255f72fd86c06d9215c9c33
glendalidelgado <5363@holbertonschool.com>
2022-08-18 03:19:58 +0000

    fixing the function

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * print_number - function with one argument
- * @n: int type argument
- *
- * Description: prints an integer
- * Return: value of n
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
+		num = -num;
 	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
